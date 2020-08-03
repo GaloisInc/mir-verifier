d3c3922c484f16f81b4dc6a156da5034f5eb468a
Stuart Pernsteiner <spernsteiner@galois.com>
2020-08-03 10:15:21 -0700

    less ambiguous branch reporting

--- test/symb_eval/crux/coverage_match.rs
+++ test/symb_eval/crux/coverage_match.rs
@@ -6,13 +6,13 @@ enum E { A, B, C }
 #[cfg_attr(crux, crux_test)]
 fn crux_test() -> i32 {
     let x = u8::symbolic("x");
+    crucible_assume!(x != 1);
     let e = match x {
         0 => E::A,
         1 => E::B,
         2 => E::C,
         _ => crucible_assume_unreachable!(),
     };
-    crucible_assume!(x != 1);
 
     let y = match e {
         E::A => 1,
